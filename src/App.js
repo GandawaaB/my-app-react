1b8d0118d048134db99ad76a3a6936e1165ecc9f
Gandavaa <baasanhvvgandawaa111@gmail.com>
2023-12-08 01:56:34 +0800

    last commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      
+        <h1 >
+          Gandavaa's Demo Page 
+        </h1>
+        <p>
+          IT infrastructure  architecture F.IT 303
+        </p>
+        <p>
+          React 
+        </p>
+       
+      </header> 
+    </div>
+  );
+}
+
+export default App;
